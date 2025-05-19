24f69f60ac441411ad992fa2b662b76e0573e23e
Crxaw <48805031+sitescript@users.noreply.github.com>
2025-05-19 21:39:02 +0100

    https://github.com/Equicord/Equicord/commit/d3ab193

--- index.tsx
+++ index.tsx
@@ -23,12 +23,6 @@ const settings = definePluginSettings({
         type: OptionType.BOOLEAN,
         default: false,
     },
-    /* enableWallpaperStyles: {
-        description: "Doensn't have to be enabled and might cause issues such as embeds being transparent if used with a non-Nitro color theme",
-        type: OptionType.BOOLEAN,
-        default: false,
-        restartNeeded: true,
-    },*/
     globalDefault: {
         description: "Set a global default wallpaper for all channels.",
         type: OptionType.COMPONENT,
@@ -41,14 +35,6 @@ export default definePlugin({
     authors: [Devs.Joona],
     description: "Use the DM wallpapers anywhere or set a custom wallpaper",
     patches: [
-        /* {
-            find: ".handleSendMessage,onResize",
-            predicate: () => settings.store.enableWallpaperStyles,
-            replacement: {
-                match: /(?:\i\?)?(null==\i\?)void 0(:\i\.isViewable)(?::void 0)?/g,
-                replace: "true"
-            },
-        },*/
         {
             find: ".wallpaperContainer,",
             group: true,
@@ -62,8 +48,8 @@ export default definePlugin({
                     replace: "$&vcWpFreeCustom||"
                 },
                 {
-                    match: /(\i=)(.{1,50}""\))/,
-                    replace: "$1arguments[0].chatWallpaperState.vcWallpaperUrl||$2"
+                    match: /(\i=).{0,25}\.asset\):null/,
+                    replace: "$1arguments[0].chatWallpaperState.vcWallpaperUrl"
                 },
                 {
                     match: /(\i\.isViewable&&)(null!=\i)/,
